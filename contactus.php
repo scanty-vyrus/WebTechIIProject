b09c9d73eb10ff5c27ad5bf41b322f596c8e8cb1
Cyrus William D'Lima <46704193+scanty-vyrus@users.noreply.github.com>
2020-04-17 10:59:50 +0530

    Add files via upload

---
+++ contactus.php
@@ -0,0 +1,76 @@
+<html>
+<head>
+<title>Contact Us</title>
+<link rel="stylesheet" type="text/css" href="cssfile.css">
+</head>
+<body background="images/pic11.jpg">
+<div class="container">
+<div id="ed"><h3>=CONTACT US=</h3></div>
+	 <table cellspacing="20" id="contact" align="center">
+	<caption><h3>DEVELOPERS</h3></caption>
+	<tr>
+		<td>
+			<img src="images/cyrus.jpg" class="circ">
+		</td>
+		<td>
+			<img src="images/khamar.png" class="circ">
+		</td>
+		<td>
+			<img src="images/roshan.jpg" class="circ">
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<h2><b>Cyrus D'lima</b></h2>
+		</td>
+		<td>
+			<h2><b>Khamaroddin Sheikh</b></h2>
+		</td>
+		<td>
+			<h2><b>Roshan Bhaskar Poojary</b></h2>
+		</td>
+	</tr>
+	<tr>
+		<td align="left">
+			<img src="images/call.png" class="log"> +91-9964951006
+		</td>
+		<td align="left">
+			<img src="images/call.png" class="log"> +91-7348905556
+		</td align="left">
+		<td align="left">
+			<img src="images/call.png" class="log"> +91-9481967218
+	</tr>
+	<tr>
+		<td align="left">
+			<img src="images/mail.jpg" class="log"> cyrusdlima@gmail.com
+		</td>
+		<td align="left">
+			<img src="images/mail.jpg" class="log"> khamaroddinsheikh@gmail.com
+		</td align="left">
+		<td align="left">
+			<img src="images/mail.jpg" class="log"> roshanpoojary@gmail.com
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<a href="https://www.facebook.com/cyrus.dlima"><img src="images/fb.png" class="log"></a>      <a href="https://in.linkedin.com/in/cyrusdlima"><img src="images/linkedin.png" class="log"></a>
+		</td>
+		<td>
+			<a href="https://www.facebook.com/khamar"><img src="images/fb.png" class="log"></a>       <a href="https://in.linkedin.com/in/khamaroddin-sheikh"><img src="images/linkedin.png" class="log"></a>
+		</td>
+		<td>
+			<a href="https://www.facebook.com/aishwarya.chandak"><img src="images/fb.png" class="log"></a>      <a href="https://in.linkedin.com/in/aishwarya-chandak"><img src="images/linkedin.png" class="log"></a>
+		</td>
+	</tr>
+	</table>
+<footer style="bottom:0; width: 100%;">
+<br>
+<a href="bidbuyuse.php">AUCTION HOME </a>
+|<a href="winners.php"> MEET WINNERS </a>
+|<a href="about.php"> ABOUT US </a>
+|<a href="contactus.php">CONTACT US</a>
+<h5>Copyright 2020  Bidbuyuse Corporation. All rights reserved.
+</h5>
+</footer>
+</body>
+</html>
